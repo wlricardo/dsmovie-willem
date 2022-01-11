3bf5ca76b3b64a0381fd9d3140c329015f73d7ec
wlricardo <wlricardo@gmail.com>
2022-01-11 10:36:21 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,7 @@
-function Listing(){
-    return (
-        <h1>Página de listagem</h1>
-    );
+import Pagination from "components/Pagination";
+
+function Listing() {
+  return <Pagination />;
 }
 
 export default Listing;
